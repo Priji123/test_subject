499bd201d93fd04a877adc5f37d1340c162cbbe6
George Priji Promod <georgepriji@gmail.com>
2022-02-16 14:45:35 +0530

    function name changed

--- factorialrc.c
+++ factorialrc.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-int factorial(int a){
+int factorial_recursive(int a){
     if(a == 1 || a == 0)
         return 1;
     else
-        return a * factorial(a-1);
+        return a * factorial_recursive(a-1);
 }
 
 int main(){
@@ -12,6 +12,6 @@ int main(){
     printf("This is a c program for factorial\n");
     printf("Enter a number for factorial but not a negative integer\n");
     scanf("%d",&a);
-    printf("The factorial of the given number is %d is %d\n", a, factorial(a));
+    printf("The factorial of the given number is %d is %d\n", a, factorial_recursive(a));
 }
 
